73756abdcd7647cc63b53a8c448e6b6e75f3f0e1
javier123454321 <javier.e.glz@gmail.com>
2019-09-20 16:31:41 -0500

    Setting up basic modal functionality

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,11 @@
 
-
-
+// Setting up the menu bar to search for Portfolio projects based on category
 function setInactive(button){
-  // console.log('set inactive called');
-  // console.log(button);
   if (button.getAttribute('class') != 'button active'){
     document.getElementsByClassName('button active')[0].setAttribute('class', 'button')
     button.setAttribute('class', 'button active')
-  }}
-
-
+  }
+}
 
 let buttons = document.getElementsByClassName("button");
 
@@ -24,10 +20,41 @@ function clickToggleActive(buttons){
   };
  }
 
- function narrowPortfolio(buttonName){
-   if (buttonName == 'allButton'){
-     
-   }
- }
-
 clickToggleActive(buttons);
+
+function narrowPortfolio(buttonName){
+  if (buttonName == 'allButton'){
+
+  }
+}
+
+
+function setModals(){
+
+  var btns = document.getElementsByClassName("portfolioProject");
+  var closeModal = document.getElementsByClassName("close");
+  for (i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function(){
+      console.log(this.parentElement.nextElementSibling);
+      this.parentElement.nextElementSibling.style.display = "block";
+    })
+
+    closeModal[i].addEventListener('click', function(){
+      this.parentElement.parentElement.parentElement.style.display = "none";
+    })
+    
+    window.onclick = function(event) {
+      let modal = document.getElementsByClassName("modal")
+      for (i = 0; i < modal.length; i++) {
+        if (event.target == modal[i]) {
+          modal[i].style.display = "none";
+        }
+      }
+    }
+
+  }
+}
+
+
+
+setModals();
